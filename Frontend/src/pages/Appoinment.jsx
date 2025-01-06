333b8a97fa9f19e2e77262e74852a7c0a511f4ed
Yashara Induwara <125277626+YGYInduwara@users.noreply.github.com>
2025-01-06 21:53:22 +0530

    ll

--- Frontend/src/pages/Appoinment.jsx
+++ Frontend/src/pages/Appoinment.jsx
@@ -56,7 +56,7 @@ const Appoinment = () => {
   </button>
 </div>
 {/**-------------------Listing Related Teams---------------- */}
-<RelatedTeams docId={docId} speciality={docInfo.speciality}/>
+<RelatedTeams docId={docId} speciality={docInfo.degree}/>
         
     </div>
   )
